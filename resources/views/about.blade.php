cfc0b6053c9cd94c416612e6d158abb385dd2afa
Nslkh <olluzran@gmail.com>
2021-04-05 16:43:20 +0500

    forelse

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,12 +8,10 @@
 </head>
 <body>
 
-  @if (count($products))
-    @foreach ($products as $product)
-      {{ $product }}
-    @endforeach
-  @else
-  <p>No any products to show</p>
-  @endif
+  @forelse ($products as $product)
+    {{ $product }}
+  @empty
+  <p>No any products to show</p>      
+  @endforelse
 </body>
 </html>

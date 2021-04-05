611203f66c8761cd1d1017cd14e5f7ca24e41789
Nslkh <olluzran@gmail.com>
2021-04-05 15:26:46 +0500

    routing with name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
-        <a href="/about">
+        <a href="{{ route('about') }}">
             Click here
         </a>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/about', "TestController@index");
+Route::get('/about-us', "TestController@index")->name('about');
